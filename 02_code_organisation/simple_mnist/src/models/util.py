4c136c53f67bfac2e50289178d339b5aacda2be7
Peter Groth <petermoerchgroth@gmail.com>
2021-06-07 12:53:40 +0200

    cookiecutter exercise

---
+++ 02_code_organisation/simple_mnist/src/models/util.py
@@ -0,0 +1,16 @@
+from torchvision import datasets, transforms
+
+def mnist():
+    # Convert to tensors and normalise
+
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,)),
+                                    ])
+
+    # Download and load the train data
+    trainset = datasets.MNIST('data/processed/MNIST_data/', download=False, train=True, transform=transform)
+
+    # Download and load the test data
+    testset = datasets.MNIST('data/processed/MNIST_data/', download=False, train=False, transform=transform)
+
+    return trainset, testset
